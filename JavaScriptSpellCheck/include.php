904629fb9c38f8e32b05153c68d764271989f2da
jcndoitt <johnng@doitt.nyc.gov>
2017-04-14 14:38:37 -0400

    include include.php

--- JavaScriptSpellCheck/include.php
+++ JavaScriptSpellCheck/include.php
@@ -1,5 +1,5 @@
 <?php
-    header("Access-Control-Allow-Origin: https://nnycmspellcheck.herokuapp.com");
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
     header("Access-Control-Max-Age: 86400");
