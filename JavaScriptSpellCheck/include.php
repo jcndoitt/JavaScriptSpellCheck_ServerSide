b06d2265768b04fb37501807b19e46f3063be83c
jcndoitt <johnng@doitt.nyc.gov>
2017-04-14 15:02:05 -0400

    include include.php

--- JavaScriptSpellCheck/include.php
+++ JavaScriptSpellCheck/include.php
@@ -5,8 +5,8 @@
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Max-Age: 86400");
     header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization");
- */
     header("Access-Control-Allow-Headers: X-Requested-With");
+ */
     include("include.js");
 ?>
 
